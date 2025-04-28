d655b6b2048a5dc71bf81335a60ebf7a0d07eba0
陈 玄 <novacx0222@qq.com>
2025-04-28 16:31:26 -0400

    Initialize CS 516 Mini Amazon Sub-Project

---
+++ src/main/java/net/hifoss/rag/MiniAmazonRagApplication.java
@@ -0,0 +1,11 @@
+package net.hifoss.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAmazonRagApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MiniAmazonRagApplication.class, args);
+    }
+}
---
+++ src/test/java/net/hifoss/rag/MiniAmazonRagApplicationTests.java
@@ -0,0 +1,11 @@
+package net.hifoss.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAmazonRagApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
